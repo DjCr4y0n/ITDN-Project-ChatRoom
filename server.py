f70f3e20cff401345a2149e1f7e0e45479b91646
DjCr4y0n <83771077+DjCr4y0n@users.noreply.github.com>
2025-11-27 17:19:37 +0000

    initial server commit for test

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = ''          # nasłuchuj na wszystkich interfejsach
+PORT = 5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on port {PORT}...")
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("Received:", data.decode())
+            conn.sendall(b"Echo: " + data)
